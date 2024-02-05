93b97040675bfe2341677e0c4d9d48a05eb7a961
Fariskt <fariskt00@gmail.com>
2024-02-05 13:13:16 +0530

    file changed

--- js/Hero.js
+++ js/Hero.js
@@ -1,24 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll('.navbar a');
-
-    links.forEach(link => {
-        link.addEventListener('click', smoothScroll);
-    });
-
-    function smoothScroll(e) {
+    const smoothScroll = function (e) {
         e.preventDefault();
-
         const targetId = this.getAttribute('href');
         const targetElement = document.querySelector(targetId);
 
         if (targetElement) {
-            window.scrollTo({
-                top: targetElement.offsetTop - 60,
-                behavior: 'smooth'
-            });
+            window.scrollTo({ top: targetElement.offsetTop - 60, behavior: 'smooth' });
         } else {
-            // If the target is on a different page, navigate to that page
             window.location.href = this.getAttribute('href');
         }
+    };
+
+    document.querySelectorAll('.nav-list li a , .welcome-container a').forEach(link => {
+        link.addEventListener('click', function (e) {
+            smoothScroll.call(this, e);
+        });
+    });
+
+    const currentLocation = window.location.hash;
+    if (currentLocation) {
+        const currentLink = document.querySelector(`.nav-list li a[href='${currentLocation}']`);
+        if (currentLink) {
+            setActiveClass(currentLink);
+        }
     }
+
+    // const navToggle = document.getElementById('nav-toggle');
+    // if (navToggle) {
+    //     navToggle.addEventListener('click', function () {
+    //         const navUl = document.querySelector('.nav-list');
+    //         if (navUl) navUl.classList.toggle('active');
+    //         this.classList.toggle('active');
+    //     });
+    // }
 });
